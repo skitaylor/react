cad818091f7317b5940f2ce8b368d85575b5b7fd
skitaylor <c0n_art@naver.com>
2024-11-14 09:40:18 +0900

    보드프로젝트2

--- board_project/frontend/src/App.jsx
+++ board_project/frontend/src/App.jsx
@@ -1,13 +1,20 @@
 import "./App.css";
 import Header from "./components/Header";
-import { BrowserRouter } from "react-router-dom";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import List from "./pages/List";
+import Save from "./pages/Save";
 function App() {
   return (
     <>
       <h2>Board Project</h2>
       <BrowserRouter>
         <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/save" element={<Save />} />
+          <Route path="/list" element={<List />} />
+        </Routes>
       </BrowserRouter>
     </>
   );
---
+++ board_project/frontend/src/pages/Save.jsx
@@ -0,0 +1,8 @@
+const Save = () => {
+  return (
+    <>
+      <h2>Save.jsx</h2>
+    </>
+  );
+};
+export default Save;
