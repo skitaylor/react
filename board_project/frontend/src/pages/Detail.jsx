f4d118f434b8c7e6fd7c8373431e1b3f7441516f
skitaylor <c0n_art@naver.com>
2024-11-19 09:28:43 +0900

    수정하기

--- board_project/frontend/src/pages/Detail.jsx
+++ board_project/frontend/src/pages/Detail.jsx
@@ -1,10 +1,12 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 import { useEffect, useState } from "react";
 import axios from "axios";
 export default function Detail() {
   const { id } = useParams();
   const [board, setBoard] = useState("");
+  const [showPass, setShowPass] = useState(false);
+  const [password, setPassword] = useState("");
   useEffect(() => {
     const board_db = axios
       .get(`http://localhost:8000/board/${id}`)
@@ -13,6 +15,22 @@ export default function Detail() {
         setBoard(res.data[0]);
       });
   }, []);
+  const updateReq = () => {
+    setShowPass(true);
+  };
+  const passInput = (e) => {
+    const { name, value } = e.target;
+    setPassword(value);
+  };
+
+  const navigate = useNavigate();
+  const passCheck = () => {
+    if (password == board.boardPass) {
+      navigate(`/update/${board.id}`);
+    } else {
+      alert("비밀번호 불일치");
+    }
+  };
   return (
     <>
       <h2>Detail.jsx</h2>
@@ -40,6 +58,13 @@ export default function Detail() {
           </tr>
         </thead>
       </table>
+      <button onClick={updateReq}>수정</button>
+      {showPass && (
+        <div>
+          <input type="text" value={password} onChange={passInput} />
+          <button onClick={passCheck}>확인</button>
+        </div>
+      )}
     </>
   );
 }
